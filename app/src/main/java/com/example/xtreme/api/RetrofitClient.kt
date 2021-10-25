be856fac713e946d1b647a519a860d44a6172615
tejasr7 <39103436+tejasr7@users.noreply.github.com>
2021-10-25 20:41:13 +0530

    Update RetrofitClient.kt

--- app/src/main/java/com/example/xtreme/api/RetrofitClient.kt
+++ app/src/main/java/com/example/xtreme/api/RetrofitClient.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private  val BASE_URL = "http://ec2-13-126-206-138.ap-south-1.compute.amazonaws.com/"
-    private val AUTH = "Basic " + Base64.encodeToString(("admin:Tejasranveer7").toByteArray(), Base64.NO_WRAP)
+    private val AUTH = "Basic " + Base64.encodeToString(("admin:").toByteArray(), Base64.NO_WRAP)
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor { chain ->
@@ -37,54 +37,5 @@ object RetrofitClient {
     }
 }
 
-//class RetrofitClient(){
-//
-//    private val BASE_URL = "http://ec2-13-126-206-138.ap-south-1.compute.amazonaws.com/v1"
-//
-//    private var mInstance: RetrofitClient? = null
-//    private var retrofit: Retrofit? = null
-//
-////    init {
-////        retrofit = Retrofit.Builder()
-////            .baseUrl(BASE_URL)
-////            .addConverterFactory(GsonConverterFactory.create())
-////            //.client(okHttpClient)
-////            .build()
-////    }
-//      private fun RetrofitClient(): RetrofitClient? {
-////        val okHttpClient = OkHttpClient.Builder()
-////            .addInterceptor(
-////                object : Interceptor() {
-////                    @Throws(IOException::class)
-////                    override fun intercept(chain: Interceptor.Chain): Response? {
-////                        val original: Request = chain.request()
-////                        val requestBuilder: Request.Builder = original.newBuilder()
-////                            .addHeader("Authorization", AUTH)
-////                            .method(original.method(), original.body())
-////                        val request: Request = requestBuilder.build()
-////                        return chain.proceed(request)
-////                    }
-////                }
-////            ).build()
-//        retrofit = Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            //.client(okHttpClient)
-//            .build()
-//    return RetrofitClient()
-//    }
-//
-//    @Synchronized
-//    fun getInstance(): RetrofitClient? {
-//        if (mInstance == null) {
-//            mInstance = RetrofitClient()
-//        }
-//        return mInstance
-//    }
-//
-//    fun getApi(): ApiInterface? {
-//        return retrofit?.create(ApiInterface::class.java)
-//    }
-//
-//}
+
 
